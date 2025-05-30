070568ea1d52ef613d97fd7165cac0f2429deaa1
Marlon Marcon <marlonmarcon@utfpr.edu.br>
2025-05-30 14:44:43 -0300

    Criação da Branch develop e adição de modelos LLM
    Adição de componente para escolha de qual modelo será utilizado para geração da resposta;
    Configuração dos modelos seguindo a API da OpenRouter, adição da API key free

--- rag.py
+++ rag.py
@@ -1,9 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-from config import OPENAI_API_KEY
+from config import OPENAI_API_KEY, OPENROUTER_API_KEY
 from prompt import template
+from openai import OpenAI
 
-def gerar_resposta(user_query, db):
+def gerar_resposta(user_query, db, modelo="gpt-4o-mini"):
     context = db.similarity_search_with_relevance_scores(user_query, k=3)
     context = list(filter(lambda x: x[1] >= 0.5, context))
 
@@ -15,5 +16,28 @@ def gerar_resposta(user_query, db):
         for k, doc in enumerate(context, start=1)
     ])
 
-    chain = template | ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=OPENAI_API_KEY) | StrOutputParser()
-    return chain.invoke({"contexto": contexto_formatado, "pergunta": user_query})
+    # Seleção do modelo e chave apropriada
+    if modelo.startswith("gpt"):
+        chain = template | ChatOpenAI(model=modelo, temperature=0, api_key=OPENAI_API_KEY) | StrOutputParser()
+        return chain.invoke({"contexto": contexto_formatado, "pergunta": user_query})
+
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENROUTER_API_KEY,
+    )
+
+    completion = client.chat.completions.create(
+    extra_headers={
+        "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+        "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+    },
+    model=modelo,
+    messages=[
+        {
+        "role": "user",
+        "content": f"Contexto:\n{contexto_formatado}\n\nPergunta: {user_query}"
+        }
+    ]
+    )
+
+    return completion.choices[0].message.content
